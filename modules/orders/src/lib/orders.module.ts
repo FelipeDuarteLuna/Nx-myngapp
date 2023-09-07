5e88ef55cc0a81792252deaea8fc68deab5cbf82
felipe.luna <felipinho.luna@gmail.com>
2023-09-07 18:42:29 -0300

    feat(Lib/Component): add star rating component

--- modules/orders/src/lib/orders.module.ts
+++ modules/orders/src/lib/orders.module.ts
@@ -1,13 +1,20 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+
 import { ordersRoutes } from './lib.routes';
 import { OrderListComponent } from './order-list/order-list.component';
-import { UiComponent } from 'modules/shared/ui';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { StarRatingComponent } from 'modules/shared/ui/src/lib/star-rating/star-rating.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(ordersRoutes),
-    UiComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(ordersRoutes),
+    StarRatingComponent,
+    ReactiveFormsModule,
+  ],
   declarations: [OrderListComponent],
 })
 export class OrdersModule {}
